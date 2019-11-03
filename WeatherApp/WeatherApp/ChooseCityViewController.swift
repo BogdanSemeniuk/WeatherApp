0a5b97c3f2213fa8586657a7b2f234bc7ea6c864
Bogdan Semeniuk <000sbv000@gmail.com>
2019-11-03 12:26:32 +0200

    add project

---
+++ WeatherApp/WeatherApp/ChooseCityViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ChooseCityViewController.swift
+//  WeatherApp
+//
+//  Created by My profile on 03.11.2019.
+//  Copyright © 2019 Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class ChooseCityViewController: UIViewController {
+}

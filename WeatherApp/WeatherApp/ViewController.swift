9778ea62d27d6350a4f3510f741c0e5b4dce793b
Bogdan Semeniuk <000sbv000@gmail.com>
2019-11-03 12:20:43 +0200

    initial commit

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by My profile on 03.11.2019.
+//  Copyright © 2019 Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
